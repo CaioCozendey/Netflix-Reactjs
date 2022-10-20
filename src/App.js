31b23e40f80ef00ca1ae4b075e637c3b1d68fc60
Caio Dias <caiocozendey@hotmail.com>
2022-10-20 19:09:58 -0300

    Criando banner. Ainda sem CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import categorias from './api';
+import categories from './api';
 import './App.css';
 import Row from './components/Row';
+import Banner from './components/Banner'
 
-export default () =>{
-  return (
-    <div className="App">
-      {/* Navbar */}
-      {/* Destaque */}
-      {/* Em alta */}
-      {categorias.map((category) => {
-        return (
-          <Row 
-            key={category.name} 
-            title={category.title} 
-            path={category.path}
-          />
-        );
-      })}
-    </div>
-  );
+export default () => {
+   return (
+      <div className="App">
+         {/* Navbar */}
+         {/* Destaque */}
+         <Banner />
+         {categories.map((category) => {
+            return (
+               <Row
+                  key={category.name}
+                  title={category.title}
+                  path={category.path}
+                  isLarge={category.isLarge}
+               />
+            );
+         })}
+      </div>
+   );
 }
-
-const url = "https://api.themoviedb.org/3/movie/550?api_key=1c86c74000988826f87edffcca8bba40"
--- src/api.js
+++ src/api.js
@@ -1,47 +1,53 @@
 const API_KEY = "1c86c74000988826f87edffcca8bba40"
 
-const categorias = [
-    {
-        name: "trending",
-        title: "Em alta", 
-        path: "/trending/all/week?api_key=${API_KEY}&language=pt-BR"
-    },
-    {
-        name: "netflixOriginals",
-        title: "Originais Netflix", 
-        path: "/discover/tv?api_key=${API_KEY}&with_networks=213"
-    },
-    {
-        name: "topRateds",
-        title: "Populares", 
-        path: "/movie/top_rated?api_key=${API_KEY}&language=pt-BR"
-    },
-    {
-        name: "comedy",
-        title: "Comédias", 
-        path: "/discover/tv?api_key=${API_KEY}&with_genres=35"
-    }, 
-    {
-        name: "romance",
-        title: "Romances", 
-        path: "/discover/tv?api_key=${API_KEY}&with_genres=10749"
-    },
-    {
-        name: "documentaries",
-        title: "Documentários", 
-        path: "/discover/tv?api_key=${API_KEY}&with_genres=99"
-    },
+const categories = [
+   {
+      name: "trending",
+      title: "Em alta",
+      path: `/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
+      isLarge: true,
+   },
+   {
+      name: "netflixOriginals",
+      title: "Originais Netflix",
+      path: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+      isLarge: false,
+   },
+   {
+      name: "topRateds",
+      title: "Populares",
+      path: `/movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
+      isLarge: false,
+   },
+   {
+      name: "comedy",
+      title: "Comédias",
+      path: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+      isLarge: false,
+   },
+   {
+      name: "romance",
+      title: "Romances",
+      path: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+      isLarge: false,
+   },
+   {
+      name: "documentaries",
+      title: "Documentários",
+      path: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+      isLarge: false,
+   },
 ];
 
 export const getMovie = async (path) => {
-    try {
-        let url = `https://api.themovied.org/3/${path}`;
-        const response = await fetch(url);
-        return await response.json();
-    } 
-    catch (error) {
-        console.log("errro getMovie", error);
-    }
+   try {
+      let url = `https://api.themoviedb.org/3${path}`;
+      const response = await fetch(url);
+      return await response.json();
+   }
+   catch (error) {
+      console.log("errro getMovie", error);
+   }
 };
 
-export default categorias;
+export default categories;
